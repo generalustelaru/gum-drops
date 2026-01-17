501c469d0d7a1da8a6825faff2ba0dbcd5dfd89d
generalustelaru <adrianpope@protonmail.com>
2026-01-17 13:30:04 +0200

    Set up stage and layer

--- KonvaService.js
+++ KonvaService.js
@@ -1,19 +1,58 @@
 import Konva from 'https://cdn.skypack.dev/konva';
 
-export class KonvaService {
-    stage;
+const stageConstants = { width: 800, height: 1200 }
 
+export class KonvaService {
     constructor() {
-        console.log('constructing');
+
+        const { width, height, scale } = this.calculateDimensions();
+
         this.stage = new Konva.Stage({
             container: 'konva-container',
-            visible: true,
-            opacity: 1,
-            ...{ width: 100, height: 100},
+            width,
+            height,
         });
+
+        this.stage.scale({ x: scale, y: scale });
+
+        this.stage.add(new Konva.Layer());
+
+        this.containerGroup = new Konva.Group({...stageConstants});
+        this.stage.getLayers()[0].add(this.containerGroup);
+
+        const background = new Konva.Rect({...stageConstants, fill:'red'})
+
+        this.containerGroup.add(background);
+
     }
 
-    greet() {
-        console.log('hi from konva')
+    calculateDimensions() {
+        const {
+            width: conainerWidth,
+            height: containerHeight,
+        } = (() => {
+            const container = document.getElementById('konva-container')?.getBoundingClientRect();
+
+            if (!container)
+                throw new Error('Cannot find canvas container!');
+
+            return container;
+        })();
+
+        if (containerHeight == 0 || conainerWidth == 0) {
+            console.error({ elementHeight: containerHeight, elementWidth: conainerWidth });
+            throw new Error('Container size is incompatible!');
+        }
+
+        const { width: sceneWidth, height: sceneHeight } = stageConstants;
+        const widthScale = conainerWidth / sceneWidth;
+        const heightScale = containerHeight / sceneHeight;
+        const scale = Math.min(widthScale, heightScale);
+
+        return {
+            width: sceneWidth * scale,
+            height: sceneHeight * scale,
+            scale,
+        };
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 import { KonvaService } from "./KonvaService.js"
 
 const konva = new KonvaService();
-
-konva.greet();
