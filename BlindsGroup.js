d7067e4dadbb6c8199c413c52dd860c51f99489d
generalustelaru <adrianpope@protonmail.com>
2026-01-17 14:23:15 +0200

    Disabled scaling

---
+++ BlindsGroup.js
@@ -0,0 +1,35 @@
+export class BlindsGroup {
+
+    constructor(
+        Konva, stage, layerId,
+    ) {
+        const width = stage.width();
+        const height = stage.height();
+
+        this.node = new Konva.Group({
+            width,
+            height,
+        });
+
+        const blindHeight = 100;
+
+        const blindConfig = {
+            width,
+            height: blindHeight,
+            fill: 'red',
+        }
+
+        const topBlind = new Konva.Rect({
+            ...blindConfig,
+        });
+
+        const bottomBlind = new Konva.Rect({
+            ...blindConfig,
+            y: stage.height() - blindHeight,
+        });
+
+        this.node.add(topBlind, bottomBlind);
+
+        stage.getLayers()[layerId].add(this.node);
+    };
+}
--- KonvaService.js
+++ KonvaService.js
@@ -1,34 +1,41 @@
 import Konva from 'https://cdn.skypack.dev/konva';
+import { BlindsGroup } from './BlindsGroup.js';
 
-const stageConstants = { width: 800, height: 1200 }
+const defaultDimensions = {
+    width: 400,
+    height: 600
+}
 
+const layers = {
+    shapes: 0,
+    blinds: 1,
+}
 export class KonvaService {
     constructor() {
 
-        const { width, height, scale } = this.calculateDimensions();
+        // const { width, height, scale } = this.calculateDimensions();
 
         this.stage = new Konva.Stage({
             container: 'konva-container',
-            width,
-            height,
+            ...defaultDimensions,
         });
 
-        this.stage.scale({ x: scale, y: scale });
+        // this.stage.scale({ x: scale, y: scale });
 
-        this.stage.add(new Konva.Layer());
+        this.stage.add(...[
+            new Konva.Layer(),
+            new Konva.Layer(),
+        ]);
 
-        this.containerGroup = new Konva.Group({...stageConstants});
-        this.stage.getLayers()[0].add(this.containerGroup);
+        this.shapeGroup = new Konva.Group({...defaultDimensions});
+        this.stage.getLayers()[layers.shapes].add(this.shapeGroup);
 
-        const background = new Konva.Rect({...stageConstants, fill:'red'})
-
-        this.containerGroup.add(background);
-
+        new BlindsGroup(Konva, this.stage, layers.blinds);
     }
 
     calculateDimensions() {
         const {
-            width: conainerWidth,
+            width: containerWidth,
             height: containerHeight,
         } = (() => {
             const container = document.getElementById('konva-container')?.getBoundingClientRect();
@@ -39,19 +46,22 @@ export class KonvaService {
             return container;
         })();
 
-        if (containerHeight == 0 || conainerWidth == 0) {
-            console.error({ elementHeight: containerHeight, elementWidth: conainerWidth });
+        if (containerHeight == 0 || containerWidth == 0) {
+            console.error({ elementHeight: containerHeight, elementWidth: containerWidth });
             throw new Error('Container size is incompatible!');
         }
 
-        const { width: sceneWidth, height: sceneHeight } = stageConstants;
-        const widthScale = conainerWidth / sceneWidth;
+        const { width: sceneWidth, height: sceneHeight } = defaultDimensions;
+        const widthScale = containerWidth / sceneWidth;
         const heightScale = containerHeight / sceneHeight;
         const scale = Math.min(widthScale, heightScale);
 
+        console.log({
+            conainerWidth: containerWidth, containerHeight, scale
+        })
         return {
-            width: sceneWidth * scale,
-            height: sceneHeight * scale,
+            width: sceneWidth,
+            height: sceneHeight,
             scale,
         };
     }
